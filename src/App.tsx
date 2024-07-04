b0e4d1a8ee4ba0670acd2bf6ec7eee1bdf89e443
N1ghtfa1l <m.pilipchuk2004@mail.ru>
2024-07-04 18:49:42 +0300

    update auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./component/Pages/Home";
 import Info from "./component/Info/Info";
@@ -6,30 +6,37 @@ import Player from "./component/Pages/Player/Player";
 import FilmsCatalog from "./component/FilmsCatalog/FilmsCatalog";
 import Favorit from "./component/Pages/Favorit/Favorit";
 import LoginPage from "./component/Pages/LoginPage";
-import "./css/App.css";
 import { useSelector } from "react-redux";
+import "./css/App.css";
 import { RootState } from "./component/store/store";
 
 function App() {
-  const auth = useSelector((state: RootState) => state.auth.value);
-  const isAuthenticated = localStorage.getItem('isAuthenticated')
+  const authState = useSelector((state: RootState) => state.auth.value);
+  const [isAuth, setIsAuth] = useState<boolean>(false);
+  const locale = localStorage.getItem("isAuthenticated") === "true";
+  useEffect(() => {
+    if (authState || locale) {
+      setIsAuth(true);
+    }
+  }, [authState]);
+
   return (
     <div className="App">
-      {auth || isAuthenticated ? (
+      {isAuth ? (
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<Home />} />
           <Route path="/info/:id" element={<Info />} />
           <Route path="/player/:id" element={<Player />} />
           <Route path="/filmscatalog" element={<FilmsCatalog />} />
           <Route path="/favorit" element={<Favorit />} />
+          <Route path="*" element={<Home />} />
         </Routes>
       ) : (
         <>
           <Routes>
             <Route path="/login" element={<LoginPage />} />
           </Routes>
-          <Navigate to="/login" />
+          <Navigate to={"/login"} />
         </>
       )}
     </div>
--- src/component/Login/Login.tsx
+++ src/component/Login/Login.tsx
@@ -2,29 +2,23 @@ import React from "react";
 import "./Login.css";
 import { useDispatch } from "react-redux";
 import { addAuth } from "../store/slices/slicesAuth";
-import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const navigate = useNavigate();
-
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   const auth = (e: any) => {
     e.preventDefault();
-    dispatch(addAuth(true));
+    dispatch(addAuth(true))
     localStorage.setItem("isAuthenticated", "true");
-    navigate("/");
   };
 
   return (
     <div className="LoginContainer">
       <div className="loginTitle">Войдите в аккаунт</div>
-      <form className="loginForm">
+      <form className="loginForm" onSubmit={auth}>
         <input type="text" placeholder="login" />
         <input type="password" placeholder="password" />
-        <button onClick={auth} className="btnLogin">
-          Войти
-        </button>
+        <button className="btnLogin">Войти</button>
       </form>
     </div>
   );
